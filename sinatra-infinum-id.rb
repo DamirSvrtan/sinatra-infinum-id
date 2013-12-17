bbc06189ec6d655a3a69fae4e43bb8a423fd1988
DamirSvrtan <damir.svrtan@gmail.com>
2013-12-17 17:15:29 +0100

    Added current_user_name

--- sinatra-infinum-id.rb
+++ sinatra-infinum-id.rb
@@ -9,15 +9,36 @@ configure do
   end
 end
 
+helpers do
+  
+  def current_user_name
+    if session[:user_id]
+      session[:user_id][:extra][:first_name]
+    end
+  end
+
+  def signed_in?
+    !!current_user_name
+  end
+
+end
+
+
 get '/' do
   erb :index
 end
 
 get '/auth/infinum/callback' do
   omniauth_info = request.env['omniauth.auth']
+  session[:user_id] = omniauth_info
   "Hello #{omniauth_info[:extra][:first_name]} #{omniauth_info[:extra][:last_name]}. You have successfully signed in on this supper dupper app! #{ENV['RACK_ENV']}"
 end
 
 get '/auth/failure' do
   "Potatos! Did not succeed to sign in! The Reason is: #{params[:message]}"
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect "#{OmniAuth::Strategies::Infinum.url}/users/sign_out?redirect_to=#{request.base_url}", notice: 'You have successfully signed out!'
 end
